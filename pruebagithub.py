4b2ac569525b287f05d922aedcdc8e9f447f472f
A. López <adlflpz@gmail.com>
2025-03-04 23:48:09 +0100

    Update pruebagithub.py
    Otro commit más, cambiando emails

--- pruebagithub.py
+++ pruebagithub.py
@@ -1,3 +1,4 @@
 print("Sólo para probar cambios en github")
 print("Probando push y pull")
 print("Esto está siendo editado desde la web de github directamente")
+print("Vamos con otro commit más para pruebas")
