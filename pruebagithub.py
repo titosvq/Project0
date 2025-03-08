459975b0e7a2435c2e817fa8bf47117a8a9b51e4
titosvq-mb <adlflpz@gmail.com>
2025-03-08 22:08:48 +0100

    Update pruebagithub.py

--- pruebagithub.py
+++ pruebagithub.py
@@ -3,3 +3,7 @@ print("Probando push y pull")
 print("Esto está siendo editado desde la web de github directamente")
 print("Vamos con otro commit más para pruebas")
 print("Prueba para implementar los commit al repo desde windows")
+
+nacimiento = input("Introduce tu año de nacimiento")
+edad = 2021 - int(nacimiento)
+print(f'Tu edad es {edad} años')
